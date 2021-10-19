8cab90bed1d84b021dfcf40e130955d416c7a454
TAIKI-desuyo <tyki0673@gmail.com>
2021-10-19 04:23:31 +0000

    ロンロン

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,28 +1,31 @@
 class Admin::ItemsController < ApplicationController
   def index
-    
+     @items = Item.all
   end
-  
+
   def show
-  
+
   end
-  
+
   def new
-     @item = ITEM.new
+     @item = Item.new
   end
-  
+
   def create
-     @item = ITEM.new(item_params)
+     @item = Item.new(item_params)
   end
-  
+
   def edit
-  
+
   end
-  
+
   def update
-    
+
   end
-  def book_params
-    params.require(:item).permit(:neme, :introduction,)
+
+  private
+   
+  def item_params
+    params.require(:item).permit(:neme, :introduction, :image)
   end
 end
