dce69db924f77f97fbc7283b0c9c06f60046915f
gfdsvvgf <patrick.jj@outlook.jp>
2021-10-19 10:11:55 +0000

    ジャンル機能追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,49 @@
 class Admin::GenresController < ApplicationController
-  
-  def create
-    
+
+
+  def show
+    @genre = Genre.find(params[:id])
+    @genres = Genre.all
   end
-  
+
+
+
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+
+  def edit
+    @genre = Genre.find(params[:id])
+
+  end
+
+
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+
+  end
+
+
+
+
 end
