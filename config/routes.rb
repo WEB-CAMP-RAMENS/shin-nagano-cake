41efbb3c34eb8195d6df2766988655680ab48464
2020030030 <nagomicunshang@gmail.com>
2021-10-19 04:08:34 +0000

    途中経過３

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+   scope module: :public do
+   root to: "homes#top"
+   get "customers/edit" => "customers#edit"
+   get "customers/my_page" => "customers#show"
+   patch "customers/my_page" => "customers#update"
+  end
+
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords,], controllers: {
@@ -17,7 +24,6 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
   namespace :admin do
    resources :items
    resources :genres
@@ -25,4 +31,13 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
    resources :orders
    resources :order_details
  end
+
+
+  scope module: :public do
+   resource :customers,only:[:edit,:update]
+   resources :addresses,except:[:new,:show]
+   get "customers/unsubscribe" => "customers#unsubscribe"
+   patch "customers/withdraw" => "customers#withdraw"
+  end
+
 end
