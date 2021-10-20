0ae5971570889ab4e2ecba78cabf47799f84bd70
gfdsvvgf <patrick.jj@outlook.jp>
2021-10-20 08:09:58 +0000

    顧客一覧実装開始

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,24 @@
 class Admin::CustomersController < ApplicationController
+  
+  def show
+    @admin_customer = Customers.find(params[:id])
+    @admin_customers = Customers.all
+  end
+  
+  def index
+    @admin_customer = Customers.new
+    @admin_customers = Customers.all
+  end
+  
+  
+  private
+
+  def customers_params
+    params.require(:customers).permit(:customer_id,:address,)
+
+  end
+
+
+  
+  
 end
