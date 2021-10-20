06c4c1d62dea874a472205d6a8cc1b7f2fbec154
gfdsvvgf <patrick.jj@outlook.jp>
2021-10-20 08:38:05 +0000

    製作中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -6,8 +6,27 @@ class Admin::CustomersController < ApplicationController
   end
   
   def index
-    @admin_customer = Customers.new
-    @admin_customers = Customers.all
+    @admin_customer = Customer.new
+    @admin_customers = Customer.all
+  end
+  
+  def edit
+    @admin_customer = Customer.find(params[:id])
+
+  end
+  
+  def create
+    @admin_customer = Customer.new(genre_params)
+    @admin_customer.save
+  end
+
+  def update
+    @admin_customer = Customer.find(params[:id])
+    if @admin_customer.update(customers_params)
+      redirect_to admin_genres_path
+    else
+      render "edit"
+    end
   end
   
   
