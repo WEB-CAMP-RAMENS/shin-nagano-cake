1d2969eaa976704556b2b32b162833bb3b4a48e0
ayacatt <k.saiko0224@gmail.com>
2021-10-21 12:38:23 +0000

    仮

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,28 +4,24 @@ before_action :authenticate_customer!
 
   def index
     @cart_items= current_customer.cart_items.all
-    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+    #@total = @cart_items.inject(0) { |sum, item| sum + (item.sum_of_price.to_i * add_tax_price) }
+    @total=0
+    @cart_items.each do |cart|
+      @total = @total + (cart.item.price * cart.amount) * 1.1
+    end
     @cart_item = CartItem.new
   end
 
   def create
     @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
       if @cart_item.present?
-        #cart_item.amount
+        @cart_item.update(amount: @cart_item.amount + params[:cart_item][:amount].to_i)
+        redirect_to cart_items_path
       else
         @cart_item = current_customer.cart_items.new(cart_item_params)
         @cart_item.save!
         redirect_to cart_items_path
       end
-
-      #@cart_item.save!
-      #flash[:notice] = "商品をカートに入れました"
-      #redirect_to cart_items_path
-    #else
-      #@item = Item.find(@cart_item.item_id)
-      #@genres = Genre.all
-      #flash.now[:alert] = "購入する個数を選択してください"
-      #render "public/items/show"
 
   end
 
@@ -46,10 +42,16 @@ before_action :authenticate_customer!
     redirect_to cart_items_path
   end
 
+  def destroy_all
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:amount, :item_id)
+    params.require(:cart_item).permit(:amount, :item_id, :customer_id)
   end
 
 end
