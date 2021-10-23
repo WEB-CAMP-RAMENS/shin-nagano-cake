56972fe55edb98d6a40bfaa6ed0dc498d1f631e7
TAIKI-desuyo <tyki0673@gamil.com>
2021-10-23 11:51:55 +0000

    商品ページ　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,16 @@
 class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-    attachment :image
-    belongs_to :genre
+  attachment :image
+  belongs_to :genre
 
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+
+ def add_tax_price
+    (self.price*1.10).round
+ end
 
 
 end
