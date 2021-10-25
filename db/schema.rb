b802965f56e2f5218f41dc51a5ef8794da2c66ff
2020030030 <nagomicunshang@gmail.com>
2021-10-25 06:22:14 +0000

    end

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_10_23_062944) do
-=======
-ActiveRecord::Schema.define(version: 2021_10_21_051454) do
-
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
